274ade72e8f9ff05a723fb7f99796c7e0887550d
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2021-02-02 18:12:33 +0100

    DI (dependency injection) - comment

--- src/edu/grcy/solid/dip/fix/TaskService.java
+++ src/edu/grcy/solid/dip/fix/TaskService.java
@@ -3,6 +3,13 @@ package edu.grcy.solid.dip.fix;
 public class TaskService {
     private Repository repository;
 
+    //dependency injection
+    //czyli wstrzykiwanie zależności
+    //czyli klasa nie powinna tworzyć obiektów od których zależy jej działanie
+    //powinna te obiekty dostać
+    // - w konstruktorze (np. Spring)
+    // - w setterze (np. Spring)
+    // - jakoś inaczej (np. Spring inicjuje za pomocą pola)
     public TaskService(Repository repository) {
         this.repository = repository;
     }
